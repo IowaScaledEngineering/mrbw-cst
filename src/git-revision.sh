65a918fc9a7619e512237d9fc1a45777f6fdb7c3
Michael Petersen <railfan@drgw.net>
2018-03-24 15:22:09 -0600

    Updated to handle other tags

--- src/git-revision.sh
+++ src/git-revision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-revisioncount=$(git rev-list  `git rev-list --tags --no-walk --max-count=1`..HEAD --count)
-projectversion=`git describe --tags --long`
+revisioncount=$(git rev-list  `git rev-list --tags="[0-9][.][0-9]" --no-walk --max-count=1`..HEAD --count)
+projectversion=`git describe --tags --match "[0-9][.][0-9]" --long`
 cleanversion=${projectversion%%-*}
 
 if [ -n "$(git status --porcelain . | grep -v ?? | grep -v src/mrbus)" ]
