7370c7a4a1b9565800face517f3593757bbb2e82
Michael Petersen <railfan@drgw.net>
2019-02-12 15:40:59 -0700

    Added pressure chars

--- src/cst-common.h
+++ src/cst-common.h
@@ -11,6 +11,16 @@
 #define PM_CHAR                 4
 #define AUX_CHAR                5
 
+// Pressure
+#define PRESSURE_00             0
+#define PRESSURE_01             1
+#define PRESSURE_02             2
+#define PRESSURE_03             3
+#define PRESSURE_10             4
+#define PRESSURE_11             5
+#define PRESSURE_12             6
+#define PRESSURE_13             7
+
 // Tonnage
 #define TONNAGE_TOP             6
 #define TONNAGE_BOTTOM          7
